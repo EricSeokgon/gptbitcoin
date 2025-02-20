69e929bf7c436ff805df0d767de6218ed5bdca12
hadesyi <leesk55@gmail.com>
2025-02-20 15:36:05 +0900

    .env 파일 추가 및 환경 변수 로드 기능 구현

--- rest.py
+++ rest.py
@@ -1,1 +1,8 @@
-print("hello world")
+import os
+from dotenv import load_dotenv
+
+load_dotenv()  # .env 파일을 읽어오는 함수.
+
+print(os.getenv("UPBIT_ACCESS_KEY"))
+print(os.getenv("UPBIT_SECRET_KEY"))
+print(os.getenv("OPENAI_API_KEY"))
